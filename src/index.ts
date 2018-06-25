dfa989f8533f17f24386e00a8cb2698582c29f44
Mattia Manzati <manzati.mattia@gmail.com>
2018-06-25 09:15:34 +0200

    fix prettier

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ export type Overwrite<A extends object, B extends object> = Pick<A, Exclude<keyo
 export type Diff<A extends object, K extends keyof A> = Omit<A, K> & Partial<Pick<A, K>>
 
 /**
-* Picks only the keys of a certain type
-*/
+ * Picks only the keys of a certain type
+ */
 export type KeysOfType<A extends object, B> = { [K in keyof A]: A[K] extends B ? K : never }[keyof A]
 
 /**
